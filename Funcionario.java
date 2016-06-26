a3a8b732f0d3bd4a901138543253873d4b9fa9d0
Leandro Vieira <lvieiraa@users.noreply.github.com>
2016-06-25 23:41:14 -0300

    Create Funcionario.java
    classe Funcionario

---
+++ Funcionario.java
@@ -0,0 +1,13 @@
+
+public class Funcionario extends Pessoa {
+private static String habilidadeProfissional;
+
+public static String getHabilidadeProfissional() {
+	return habilidadeProfissional;
+}
+
+public static void setHabilidadeProfissional(String habilidadeProfissional) {
+	Funcionario.habilidadeProfissional = habilidadeProfissional;
+}
+
+}
