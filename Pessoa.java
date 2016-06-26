0bdaf913ae336e2b6df6b6d0fc59e05f683c3719
Leandro Vieira <lvieiraa@users.noreply.github.com>
2016-06-25 23:40:13 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,52 @@
+
+public class Pessoa {
+	
+	
+		private static String nome, email, cpf, telefone, numIdentidade;
+		
+	public static String getNome() {
+			return nome;
+		}
+
+		public static void setNome(String nome) {
+			Pessoa.nome = nome;
+		}
+
+		public static String getEmail() {
+			return email;
+		}
+
+		public static void setEmail(String email) {
+			Pessoa.email = email;
+		}
+
+		public static String getCpf() {
+			return cpf;
+		}
+
+		public static void setCpf(String cpf) {
+			Pessoa.cpf = cpf;
+		}
+
+		public static String getTelefone() {
+			return telefone;
+		}
+
+		public static void setTelefone(String telefone) {
+			Pessoa.telefone = telefone;
+		}
+
+		public static String getNumIdentidade() {
+			return numIdentidade;
+		}
+
+		public static void setNumIdentidade(String numIdentidade) {
+			Pessoa.numIdentidade = numIdentidade;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
